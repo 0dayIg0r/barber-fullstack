4b0af3e68eb2f55e83c87cf8e901a542bf8ea9a1
0dayig0r <igormedeiros.f@gmail.com>
2025-06-02 14:54:48 -0300

    selecionando propriedade id no retorno

--- src/service/user/UserDetailService.ts
+++ src/service/user/UserDetailService.ts
@@ -1,26 +1,26 @@
-import prismaClient from '../../prisma'
-interface UserRequest{
-  user_id: string
+import prismaClient from "../../prisma";
+interface UserRequest {
+  user_id: string;
 }
 
 class UserDetailService {
-  async execute({user_id}:UserRequest) {
-    
+  async execute({ user_id }: UserRequest) {
     const user = await prismaClient.user.findFirst({
-      where:{
-        id:user_id
+      where: {
+        id: user_id,
       },
-      select:{
+      select: {
+        id: true,
         name: true,
         email: true,
         address: true,
         service: true,
         subscriptions: true,
-        created_at: true
-      }
-    })
+        created_at: true,
+      },
+    });
 
-    return user
+    return user;
   }
 }
 
