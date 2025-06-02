55162912cf510312de1e66bceda92c17f7d682ab
0dayig0r <igormedeiros.f@gmail.com>
2025-06-02 14:43:33 -0300

    retornando informações necessárias do user

--- src/controller/user/UserDetailController.ts
+++ src/controller/user/UserDetailController.ts
@@ -7,7 +7,7 @@ class UserDetailController {
 
     const userDetailService = new UserDetailService();
 
-    const detailUser = await userDetailService.execute();
+    const detailUser = await userDetailService.execute({user_id});
 
      res.json(detailUser);
      return
