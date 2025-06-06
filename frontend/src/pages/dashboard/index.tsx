c013bbb31b7106205e5aec13db8a5c25b49f318b
0dayig0r <igormedeiros.f@gmail.com>
2025-06-06 11:03:10 -0300

    protegendo rotas

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,21 +1,24 @@
-import { Flex, Text } from "@chakra-ui/react"
-import Head from "next/head"
+import { canSSRAuth } from "@/src/utils/canSSRAuth";
+import { Flex, Text } from "@chakra-ui/react";
+import Head from "next/head";
 
 function Dashboard() {
   return (
     <>
-    <Head>
-        <title>
-            Barber NINJA - Minha Barbearia
-        </title>
+      <Head>
+        <title>Barber NINJA - Minha Barbearia</title>
         <Flex>
-            <Text>
-                Bem vindo ao dashboard
-            </Text>
+          <Text>Bem vindo ao dashboard</Text>
         </Flex>
-    </Head>
+      </Head>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  return {
+    props: {},
+  };
+});
