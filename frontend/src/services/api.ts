c8416a5175e56196800ab3dc737236f45c3840da
0dayig0r <igormedeiros.f@gmail.com>
2025-06-05 21:49:59 -0300

    configurando api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies } from "nookies";
 import { AuthTokenError } from "./errors/AuthTokenError";
+import { signOut } from "../context/signOut";
 
 export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx);
@@ -19,6 +20,7 @@ export function setupAPIClient(ctx = undefined) {
     (error: AxiosError) => {
       if (error.response?.status === 401) {
         if (typeof window !== undefined) {
+            signOut()
         } else {
           return Promise.reject(new AuthTokenError());
         }
