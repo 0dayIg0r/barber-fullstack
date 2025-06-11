3649fe5f79854101765ff47d8002dab8e5263a74
0dayig0r <igormedeiros.f@gmail.com>
2025-06-11 16:00:18 -0300

    pagina e requisições

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -1,9 +1,27 @@
 import { Sidebar } from "@/src/components/sidebar";
+import { signOut } from "@/src/context/signOut";
+import { setupAPIClient } from "@/src/services/api";
+import { canSSRAuth } from "@/src/utils/canSSRAuth";
 import { Box, Button, Flex, Heading, Input, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 
-function Profile() {
+interface UserProps {
+  id: string;
+  name: string;
+  email: string;
+  address: string | null;
+}
+
+interface ProfileProps {
+  user: UserProps;
+  premium: boolean;
+}
+function Profile({ user, premium }: ProfileProps) {
+ const [name, setName] = useState(user && user?.name)
+ const [address, setAddress] = useState(user?.address === null ? 'Sem endereço, cadastre agora' : user?.address)
+
   return (
     <>
       <Head>
@@ -12,13 +30,14 @@ function Profile() {
 
       <Sidebar>
         <Flex
+        fontFamily={'body'}
           direction="column"
           align="flex-start"
           justify="flex-start"
           maxW="700px"
           w="100%"
-          mx="auto" // centraliza horizontalmente na tela
-          px={4} // padding horizontal para não encostar nas bordas da tela
+          mx="auto"
+          px={4}
         >
           <Heading
             fontSize="3xl"
@@ -34,8 +53,8 @@ function Profile() {
           <Flex
             direction="column"
             bg="barber.400"
-            p={6} // padding em todas as direções para espaçamento
-            borderRadius="md" // bordas arredondadas para suavizar visualmente
+            p={6}
+            borderRadius="md"
             w="100%"
           >
             <Text
@@ -54,12 +73,15 @@ function Profile() {
               color="white"
               _placeholder={{ color: "gray.400" }}
               border="none"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               _focus={{
                 borderColor: "barber.200",
                 boxShadow: "0 0 0 2px #D69E2E",
-              }} // foco com destaque
+              }}
               w="100%"
             />
+
             <Text
               mb={3}
               mt={3}
@@ -73,14 +95,16 @@ function Profile() {
 
             <Input
               bg="gray.900"
-              placeholder="Nome da sua barbearia"
-              color="white"
-              _placeholder={{ color: "gray.400" }}
+              placeholder="Endereço da sua barbearia"
+              color={user?.address === null ?  'orange.500' : 'white'}
+              _placeholder={{ color: "white" }}
               border="none"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
               _focus={{
                 borderColor: "barber.200",
                 boxShadow: "0 0 0 2px #D69E2E",
-              }} // foco com destaque
+              }}
               w="100%"
             />
 
@@ -94,6 +118,7 @@ function Profile() {
             >
               Plano atual:
             </Text>
+
             <Flex
               direction="row"
               w="100%"
@@ -105,8 +130,8 @@ function Profile() {
               alignItems="center"
               justifyContent="space-between"
             >
-              <Text p={2} fontSize={"lg"} color="green.500">
-                Plano Grátis
+              <Text p={2} fontSize={"lg"} color={premium ? 'gray.500' : 'orange.500'}>
+                {premium ? "Premium" : "Gratuito"}
               </Text>
               <Link href={"/planos"}>
                 <Box
@@ -117,12 +142,13 @@ function Profile() {
                   rounded={4}
                   bg={"#00cd52"}
                   color="white"
-                    fontWeight={'bolder'}
+                  fontWeight={"bolder"}
                 >
-                  Mudar plano
+                  {premium ? 'Desativar' : 'Mudar plano'}
                 </Box>
               </Link>
             </Flex>
+
             <Button
               w={"100%"}
               mt={3}
@@ -130,19 +156,21 @@ function Profile() {
               bg={"button.cta"}
               size="lg"
               borderColor={"white"}
-                fontWeight={'bolder'}
+              fontWeight={"bolder"}
               _hover={{ bg: "#ffb13e", borderColor: "#ffb13e" }}
             >
               Salvar
             </Button>
+
             <Button
               w={"100%"}
               mb={6}
               borderColor={"red.500"}
               color={"red.500"}
               size="lg"
-              fontWeight={'bolder'}
+              fontWeight={"bolder"}
               _hover={{ bg: "red.500", color: "white" }}
+              onClick={signOut}
             >
               Sair da conta
             </Button>
@@ -152,5 +180,32 @@ function Profile() {
     </>
   );
 }
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  try {
+    const apiClient = setupAPIClient(ctx);
+    const res = await apiClient.get("/me");
+
+    const user = {
+      id: res.data.id,
+      name: res.data.name,
+      email: res.data.email,
+      address: res.data?.address,
+    };
+
+    return {
+      props: {
+        user: user,
+        premium: res.data?.subscriptions?.status === "active" ? true : false,
+      },
+    };
+  } catch (err) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+});
 
 export default Profile;
