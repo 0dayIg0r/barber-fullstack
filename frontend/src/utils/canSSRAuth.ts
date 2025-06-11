f09644fe1d52e006bb715b6b9b03264b9a862fc9
0dayig0r <igormedeiros.f@gmail.com>
2025-06-11 16:01:17 -0300

    corrigindo verificação de auth, desativando strict do tsconfig.json

--- frontend/src/utils/canSSRAuth.ts
+++ frontend/src/utils/canSSRAuth.ts
@@ -1,42 +1,37 @@
-import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
-  GetServerSidePropsResult,
-} from "next";
-import { parseCookies, destroyCookie } from "nookies";
-import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import {destroyCookie, parseCookies } from 'nookies';
+import { AuthTokenError } from '../services/errors/AuthTokenError'
 
-export function canSSRAuth<P extends { [key: string]: any; }>(fn: GetServerSideProps<P>) {
-  return async (
-    ctx: GetServerSidePropsContext
-  ): Promise<GetServerSidePropsResult<P>> => {
+export function canSSRAuth<P>(fn: GetServerSideProps<P>){
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
+
     const token = cookies['@barber.token'];
 
-    if (!token) {
-      return {
-        redirect: {
-          destination: '/',
+    if(!token){
+      return{
+        redirect:{
+          destination: '/login',
           permanent: false,
-        },
-      };
+        }
+      }
     }
 
-    try {
+    try{
       return await fn(ctx);
-    } catch (err) {
-      if (err instanceof AuthTokenError) {
-        destroyCookie(ctx, '@barber.token');
+    }catch(err){
+      if(err instanceof AuthTokenError){
+        destroyCookie(ctx, '@barber.token', { path: '/' });
 
-        return {
-          redirect: {
+        return{
+          redirect:{
             destination: '/',
             permanent: false,
-          },
-        };
-      }
+          }
+        }
 
-      throw err;
+      }
     }
-  };
+
+  }
 }
