0947cec045b0424fb148eb3181e9ac861d7ff0da
0dayig0r <igormedeiros.f@gmail.com>
2025-06-17 22:05:37 -0300

    pagina de clientes

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,26 +1,72 @@
 import { Sidebar } from "@/src/components/sidebar";
 import { canSSRAuth } from "@/src/utils/canSSRAuth";
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  Link as ChakraLink,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import Link from "next/link";
+import { IoMdPerson } from "react-icons/io";
 
-function Dashboard() {
-
+export default function Dashboard() {
   return (
     <>
       <Head>
-        <title>Barber NINJA - Minha Barbearia</title>
+        <title>BarberPRO - Minha barbearia</title>
       </Head>
-
       <Sidebar>
-        <Flex>
-          <Text>Bem vindo ao dashboard</Text>
+        <Flex direction="column" align="flex-start" justify="flex-start">
+          <Flex w="100%" direction="row" align="center" justify="flex-start">
+            <Heading fontSize="3xl" mt={4} mb={4} mr={4} color={"white"}>
+              Agenda
+            </Heading>
+            <Link href="/new">
+              <Button>Registrar</Button>
+            </Link>
+          </Flex>
+
+          <ChakraLink
+            w="100%"
+            m={0}
+            p={0}
+            mt={1}
+            bg="transparent"
+            style={{ textDecoration: "none" }}
+          >
+            <Flex
+              w="100%"
+              direction={"row"}
+              p={4}
+              rounded={4}
+              mb={4}
+              bg="barber.400"
+              justify="space-between"
+              align={"center"}
+            >
+              <Flex direction="row" mb={0} align="center" justify="center">
+                <IoMdPerson size={28} color="#f1f1f1" />
+                <Text fontWeight="bold" ml={4} color={"white"}>
+                  Matheus Fraga
+                </Text>
+              </Flex>
+
+              <Text fontWeight="bold" mb={0} color={"white"}>
+                Corte completo
+              </Text>
+              <Text fontWeight="bold" mb={0} color={"white"}>
+                R$ 59.90
+              </Text>
+            </Flex>
+          </ChakraLink>
         </Flex>
       </Sidebar>
     </>
   );
 }
-
-export default Dashboard;
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
   return {
